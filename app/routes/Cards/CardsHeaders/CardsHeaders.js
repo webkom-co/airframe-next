2bae8ce59794424b8ebce136a39addc15066f4bc
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-08-20 11:49:28 +0200

    Done in section "2" in "Card Headers".

--- app/routes/Cards/CardsHeaders/CardsHeaders.js
+++ app/routes/Cards/CardsHeaders/CardsHeaders.js
@@ -6,6 +6,12 @@ import {
     Col,
     Card,
     Badge,
+    Nav,
+    NavItem,
+    NavLink,
+    TabPane,
+    UncontrolledTabs,
+    CardHeader,
     CardBody,
     CardTitle
 } from './../../../components';
@@ -193,6 +199,225 @@ const Cards = () => (
                 </Col>
             </Row>
             { /* END Section 1 */}
+
+            { /* START Header 2 */}
+            <Row>
+                <Col lg={ 12 }>
+                    <HeaderDemo 
+                        no={2} 
+                        title="Cards Headers - Navs" 
+                        className="mt-5"
+                        subTitle="Provide contextual feedback messages"
+                    />
+                </Col>
+            </Row>
+            { /* END Header 2 */}
+            { /* START Section 2 */}
+            <Row>
+                <Col lg={ 6 }>
+                    { /* START Card */}
+                    <Card className="mb-3">
+                        <UncontrolledTabs initialActiveTabId="users201a">
+                            <CardHeader>
+                                <Nav tabs className="card-header-tabs">
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="users201a">
+                                            Users
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="settings201b">
+                                            Settings
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </CardHeader>
+                            <CardBody>
+                                <UncontrolledTabs.TabContent>
+                                    <TabPane tabId="users201a">
+                                        <CardTextDemo 
+                                            cardNo="2.01a"
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="settings201b">
+                                        <CardTextDemo 
+                                            cardNo="2.01b"
+                                        />
+                                    </TabPane>
+                                </UncontrolledTabs.TabContent>
+                            </CardBody>
+                        </UncontrolledTabs>
+                    </Card>
+                    { /* END Card */}
+                    { /* START Card */}
+                    <Card className="mb-3">
+                        <UncontrolledTabs initialActiveTabId="users202a">
+                            <CardHeader>
+                                <Nav tabs className="card-header-tabs">
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="users202a">
+                                            <i className="fa fa-user mr-2"></i> Users
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="settings202b">
+                                            <i className="fa fa-gear mr-2"></i> Settings
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </CardHeader>
+                            <CardBody>
+                                <UncontrolledTabs.TabContent>
+                                    <TabPane tabId="users202a">
+                                        <CardTextDemo 
+                                            cardNo="2.02a"
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="settings202b">
+                                        <CardTextDemo 
+                                            cardNo="2.02b"
+                                        />
+                                    </TabPane>
+                                </UncontrolledTabs.TabContent>
+                            </CardBody>
+                        </UncontrolledTabs>
+                    </Card>
+                    { /* END Card */}
+                    { /* START Card */}
+                    <Card className="mb-3">
+                        <UncontrolledTabs initialActiveTabId="users203a">
+                            <CardHeader>
+                                <Nav tabs className="card-header-tabs">
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="users203a">
+                                            <i className="fa fa-user fa-fw"></i>
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="settings203b">
+                                            <i className="fa fa-gear fa-fw"></i>
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </CardHeader>
+                            <CardBody>
+                                <UncontrolledTabs.TabContent>
+                                    <TabPane tabId="users203a">
+                                        <CardTextDemo 
+                                            cardNo="2.03a"
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="settings203b">
+                                        <CardTextDemo 
+                                            cardNo="2.03b"
+                                        />
+                                    </TabPane>
+                                </UncontrolledTabs.TabContent>
+                            </CardBody>
+                        </UncontrolledTabs>
+                    </Card>
+                    { /* END Card */}
+                    { /* START Card */}
+                    <Card className="mb-3">
+                        <UncontrolledTabs initialActiveTabId="users204a">
+                            <CardBody>
+                                <Nav pills className="mb-3">
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="users204a">
+                                            Users
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="settings204b">
+                                            Settings
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                </Nav>
+                                <UncontrolledTabs.TabContent>
+                                    <TabPane tabId="users204a">
+                                        <CardTextDemo 
+                                            cardNo="2.04a"
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="settings204b">
+                                        <CardTextDemo 
+                                            cardNo="2.04b"
+                                        />
+                                    </TabPane>
+                                </UncontrolledTabs.TabContent>
+                            </CardBody>
+                        </UncontrolledTabs>
+                    </Card>
+                    { /* END Card */}
+                </Col>
+                <Col lg={ 6 }>
+                    { /* START Card */}
+                    <Card className="mb-3">
+                        <UncontrolledTabs initialActiveTabId="users205a">
+                            <CardBody>
+                                <Nav pills className="mb-3">
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="users205a">
+                                            <i className="fa fa-user mr-2"></i> Users
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="settings205b">
+                                            <i className="fa fa-gear mr-2"></i> Settings
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                </Nav>
+                                <UncontrolledTabs.TabContent>
+                                    <TabPane tabId="users205a">
+                                        <CardTextDemo 
+                                            cardNo="2.05a"
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="settings205b">
+                                        <CardTextDemo 
+                                            cardNo="2.05b"
+                                        />
+                                    </TabPane>
+                                </UncontrolledTabs.TabContent>
+                            </CardBody>
+                        </UncontrolledTabs>
+                    </Card>
+                    { /* END Card */}
+                    { /* START Card */}
+                    <Card className="mb-3">
+                        <UncontrolledTabs initialActiveTabId="users206a">
+                            <CardBody>
+                                <Nav pills className="mb-3">
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="users206a">
+                                            <i className="fa fa-user"></i>
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <UncontrolledTabs.NavLink tabId="settings206b">
+                                            <i className="fa fa-gear"></i>
+                                        </UncontrolledTabs.NavLink>
+                                    </NavItem>
+                                </Nav>
+                                <UncontrolledTabs.TabContent>
+                                    <TabPane tabId="users206a">
+                                        <CardTextDemo 
+                                            cardNo="2.06a"
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="settings206b">
+                                        <CardTextDemo 
+                                            cardNo="2.06b"
+                                        />
+                                    </TabPane>
+                                </UncontrolledTabs.TabContent>
+                            </CardBody>
+                        </UncontrolledTabs>
+                    </Card>
+                    { /* END Card */}
+                </Col>
+            </Row>
+            { /* END Section 2 */}
 
         </Container>
     </React.Fragment>
