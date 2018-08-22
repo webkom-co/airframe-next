5f79f3142f4f919c4dba5643ccc3261dbf8dff29
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-08-22 17:18:41 +0200

    Copy/paste Sidebar (Top and Bottom) from bs4.webkom.co . Done.

--- app/layout/components/SidebarDefault.js
+++ app/layout/components/SidebarDefault.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import faker from 'faker';
+import { Link } from 'react-router-dom';
 
 import {
     Sidebar,
@@ -6,10 +8,14 @@ import {
     Tools,
     Avatar,
     AvatarAddOn,
-    Progress
+    UncontrolledButtonDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from './../../components';
 
 import { SidebarNavMenu } from './SidebarNavMenu';
+import { FooterAuth } from '../../routes/components/Pages/FooterAuth';
 
 import avatarImg from './../../images/avatars/avatar-1.jpg';
 
@@ -22,31 +28,72 @@ export const SidebarDefault = () => (
         </Sidebar.Close>
 
         <Sidebar.Section>
-            { /* Avatar */ }
-            <Tools.SlimProps
-                slimProps={{ size: 'md' }}
-            >
-                <Avatar.Image
-                    size="lg"
-                    src={ avatarImg }
-                    addOns={[
-                        <AvatarAddOn.Icon 
-                            className="fa fa-circle"
-                            color="white"
-                            key="avatar-icon-bg"
-                        />,
-                        <AvatarAddOn.Icon 
-                            className="fa fa-circle"
-                            color="success"
-                            key="avatar-icon-fg"
-                        />
-                    ]}
-                />
-            </Tools.SlimProps>
+            { /* START Logo */ }
+            <Tools.DefaultOnly>
+                <div className="mb-4">
+                    <Link to="/" className="h4 fw-600 text-primary">
+                        airframe
+                    </Link>
+                </div>
+            </Tools.DefaultOnly>
+            { /* END Logo */ }
+            { /* START Avatar */ }
+            <Link to="/">
+                <Tools.SlimProps
+                    slimProps={{ size: 'md' }}
+                >
+                    <Avatar.Image
+                        size="lg"
+                        src={ avatarImg }
+                        addOns={[
+                            <AvatarAddOn.Icon 
+                                className="fa fa-circle"
+                                color="white"
+                                key="avatar-icon-bg"
+                            />,
+                            <AvatarAddOn.Icon 
+                                className="fa fa-circle"
+                                color="success"
+                                key="avatar-icon-fg"
+                            />
+                        ]}
+                    />
+                </Tools.SlimProps>
+            </Link>
+            { /* END Avatar */ }
+            <br />
             { /* User Info (non-slim only) */ }
             <Tools.DefaultOnly>
-                <h6 className="mt-2">Craig Marsh</h6>
-                <p className="mb-0">International Usability</p>
+                <UncontrolledButtonDropdown>
+                    <DropdownToggle color="link" className="pl-0 pb-0">
+                        { faker.name.firstName() } { faker.name.lastName() }
+                        <i className="fa fa-angle-down ml-2"></i>
+                    </DropdownToggle>
+                    <DropdownMenu persist>
+                    <DropdownItem header>
+                        { faker.name.firstName() } { faker.name.lastName() }
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem tag={ Link } to="/apps/profile-details">
+                        My Profile
+                    </DropdownItem>
+                    <DropdownItem tag={ Link } to="/apps/settings-edit">
+                        Settings
+                    </DropdownItem>
+                    <DropdownItem tag={ Link } to="/apps/billing-edit">
+                        Billings
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem tag={ Link } to="/pages/login">
+                        <i className="fa fa-fw fa-sign-out mr-2"></i>
+                        Sign Out
+                    </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledButtonDropdown>
+                <br />
+                <span className="small">
+                    { faker.name.jobTitle() }
+                </span>
             </Tools.DefaultOnly>
         </Sidebar.Section>
 
@@ -58,20 +105,42 @@ export const SidebarDefault = () => (
         <Sidebar.Section>
             { /* Capacity / Storage (non-slim only) */ }
             <Tools.DefaultOnly>
-                <div>
-                    <Progress color="primary" slim value={ 40 }/>
-                    <div className="d-flex justify-content-between">
-                        <span>Capacity</span>
-                        <span>40%</span>
-                    </div>
-                </div>
-                <div className="mt-3">
-                    <Progress color="warning" slim value={ 60 }/>
-                    <div className="d-flex justify-content-between">
-                        <span>Storage</span>
-                        <span>60%</span>
-                    </div>
-                </div>
+                <UncontrolledButtonDropdown direction="up" className="mb-3">
+                    <DropdownToggle color="link" className="text-left pl-0 pb-0">
+                        React 1.0.0 <i className="fa fa-angle-up ml-2"></i>
+                        <br />
+                        <span className="small">
+                            Sun, Jun 12, 2018 4:43:12 PM
+                        </span>
+                    </DropdownToggle>
+                    <DropdownMenu persist>
+                        <DropdownItem header>
+                            Choose Version
+                        </DropdownItem>
+                        <DropdownItem href="http://react.bs4.webkom.co" active>
+                            React 1.0.0
+                            <br />
+                            <span className="small">
+                                Sun, Jun 12, 2018 4:43:12 PM
+                            </span>
+                        </DropdownItem>
+                        <DropdownItem href="http://angular.bs4.webkom.co">
+                            Angular 1.0.0
+                            <br />
+                            <span className="small">
+                                Sun, Jun 12, 2018 4:43:12 PM
+                            </span>
+                        </DropdownItem>
+                        <DropdownItem href="http://bs4.webkom.co">
+                            Jquery 1.0.0
+                            <br />
+                            <span className="small">
+                                Sun, Jun 12, 2018 4:43:12 PM
+                            </span>
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledButtonDropdown>
+                <FooterAuth />
             </Tools.DefaultOnly>
         </Sidebar.Section>
     </Sidebar>
