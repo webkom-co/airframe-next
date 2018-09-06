9a8b85df88e3b08ece596b833fc35dfdc707fefe
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-09-06 11:55:06 +0200

    Added "breadcrumb" to "Navbar" (with Sidebar).

--- app/layout/components/NavbarDefault.js
+++ app/layout/components/NavbarDefault.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import {
     Navbar,
@@ -14,8 +15,27 @@ import { NavbarUser } from './NavbarUser';
 const NavbarDefault = () => (
     <Navbar light color="none" expand="xs">
         <Nav navbar>
-            <NavItem>
+            <NavItem className="mr-3">
                 <SidebarTrigger/>
+            </NavItem>
+            <NavItem>
+                <span className="navbar-text">
+                    <Link to="/">
+                        <i className="fa fa-home"></i>
+                    </Link>
+                </span>
+                <span className="navbar-text px-2">
+                    <i className="fa fa-angle-right"></i>
+                </span>
+                <span className="navbar-text">
+                    <Link to="/">Start</Link>
+                </span>
+                <span className="navbar-text px-2">
+                    <i className="fa fa-angle-right"></i>
+                </span>
+                <span className="navbar-text">
+                    Page Link
+                </span>
             </NavItem>
         </Nav>
         <Nav navbar className="ml-auto">
