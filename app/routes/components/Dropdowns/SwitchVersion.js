6d8efc318542f027f4202c0f16b28f519d83b058
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-09-07 15:33:58 +0200

    Changed "Switch Version" layout in Dropdown.

--- app/routes/components/Dropdowns/SwitchVersion.js
+++ app/routes/components/Dropdowns/SwitchVersion.js
@@ -13,25 +13,34 @@ const SwitchVersion = () => (
             <DropdownItem header>
                 Choose Version
             </DropdownItem>
-            <DropdownItem href="http://react.bs4.webkom.co" active>
-                React 1.0.0
-                <br />
-                <span className="small">
-                    Sun, Jun 12, 2018 4:43:12 PM
+            <DropdownItem href="http://v2.spin.webkom.co" className="d-flex">
+                <i className="fa fa-fw fa-circle-o align-self-center mr-2" />
+                <span>
+                    Jquery 1.0.0
+                    <br />
+                    <span className="small">
+                        Sun, Jun 12, 2018 4:43:12 PM
+                    </span>
                 </span>
             </DropdownItem>
-            <DropdownItem href="http://angular.bs4.webkom.co">
-                Angular 1.0.0
-                <br />
-                <span className="small">
-                    Sun, Jun 12, 2018 4:43:12 PM
+            <DropdownItem href="http://react.spin.webkom.co" active className="d-flex">
+                <i className="fa fa-fw fa-check align-self-center mr-2" />
+                <span>
+                    React 1.0.0
+                    <br />
+                    <span className="small">
+                        Sun, Jun 12, 2018 4:43:12 PM
+                    </span>
                 </span>
             </DropdownItem>
-            <DropdownItem href="http://bs4.webkom.co">
-                Jquery 1.0.0
-                <br />
-                <span className="small">
-                    Sun, Jun 12, 2018 4:43:12 PM
+            <DropdownItem href="http://angular.spin.webkom.co" className="d-flex">
+                <i className="fa fa-fw fa-circle-o align-self-center mr-2" />
+                <span>
+                    Angular 1.0.0
+                    <br />
+                    <span className="small">
+                        Sun, Jun 12, 2018 4:43:12 PM
+                    </span>
                 </span>
             </DropdownItem>
         </DropdownMenu>
