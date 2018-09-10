49cabf95b08cab99a91396fb04a0affb6b0c1a6f
Tomasz O <tomasz.owczarczyk@gmail.com>
2018-09-10 09:33:56 +0200

    Update SwitchVersion.js

--- app/routes/components/Dropdowns/SwitchVersion.js
+++ app/routes/components/Dropdowns/SwitchVersion.js
@@ -14,7 +14,6 @@ const SwitchVersion = () => (
                 Choose Version
             </DropdownItem>
             <DropdownItem href="http://bs4.webkom.co" className="d-flex">
-                <i className="fa fa-fw fa-circle-o align-self-center mr-2" />
                 <span>
                     Jquery 1.0.0
                     <br />
@@ -22,9 +21,9 @@ const SwitchVersion = () => (
                         Sun, Jun 12, 2018 4:43:12 PM
                     </span>
                 </span>
+                <i className="fa fa-fw ml-auto align-self-center pl-2" />
             </DropdownItem>
             <DropdownItem href="http://react.bs4.webkom.co" active className="d-flex">
-                <i className="fa fa-fw fa-check align-self-center mr-2" />
                 <span>
                     React 1.0.0
                     <br />
@@ -32,9 +31,9 @@ const SwitchVersion = () => (
                         Sun, Jun 12, 2018 4:43:12 PM
                     </span>
                 </span>
+                <i className="fa fa-fw fa-check ml-auto align-self-center pl-4" />
             </DropdownItem>
             <DropdownItem href="http://angular.bs4.webkom.co" className="d-flex">
-                <i className="fa fa-fw fa-circle-o align-self-center mr-2" />
                 <span>
                     Angular 1.0.0
                     <br />
@@ -42,6 +41,7 @@ const SwitchVersion = () => (
                         Sun, Jun 12, 2018 4:43:12 PM
                     </span>
                 </span>
+                <i className="fa fa-fw ml-auto align-self-center pl-2" />
             </DropdownItem>
         </DropdownMenu>
     </React.Fragment>
