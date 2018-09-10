ba34e7c5301b1278bb90c6638c7b6bb63c3b0b70
Tomasz O <tomasz.owczarczyk@gmail.com>
2018-09-10 11:41:20 +0200

    Update PieChartWithCustomizedLabel.js

--- app/routes/Graphs/ReCharts/components/PieChartWithCustomizedLabel.js
+++ app/routes/Graphs/ReCharts/components/PieChartWithCustomizedLabel.js
@@ -7,13 +7,15 @@ import {
     PieValueLabel
 } from './../../../../components/recharts';
 
+import colors from './../../../../colors';
+
 const data = [
     {name: 'Group A', value: 400},
     {name: 'Group B', value: 300},
     {name: 'Group C', value: 300},
     {name: 'Group D', value: 200}
 ];
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = [ colors['primary'], colors['purple'], colors['success'], colors['yellow']];
 
 export const PieChartWithCustomizedLabel = () => (
     <ResponsiveContainer width='100%' aspect={6.0/3.0}>
