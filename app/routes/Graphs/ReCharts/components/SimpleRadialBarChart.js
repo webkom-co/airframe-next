7d05e0aa3f8bcd7e0d733f524b074c38e07eec2e
Tomasz O <tomasz.owczarczyk@gmail.com>
2018-09-10 12:11:53 +0200

    Update SimpleRadialBarChart.js

--- app/routes/Graphs/ReCharts/components/SimpleRadialBarChart.js
+++ app/routes/Graphs/ReCharts/components/SimpleRadialBarChart.js
@@ -6,14 +6,16 @@ import {
     Legend
 } from 'recharts';
 
+import colors from './../../../../colors';
+
 const data = [
-      {name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8'},
-      {name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed'},
-      {name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1'},
-      {name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d'},
-      {name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c'},
-      {name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57'},
-      {name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658'}
+      {name: '18-24', uv: 31.47, pv: 2400, fill: colors['info-04']},
+      {name: '25-29', uv: 26.69, pv: 4567, fill: colors['info-07']},
+      {name: '30-34', uv: 15.69, pv: 1398, fill: colors['info']},
+      {name: '35-39', uv: 8.22, pv: 9800, fill: colors['primary-04']},
+      {name: '40-49', uv: 8.63, pv: 3908, fill: colors['primary-07']},
+      {name: '50+', uv: 2.63, pv: 4800, fill: colors['primary']},
+      {name: 'unk.', uv: 6.67, pv: 4800, fill: colors['purple']}
     ];
     
   const style = {
