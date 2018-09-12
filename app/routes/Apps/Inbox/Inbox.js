b2cfb52701f62e23ce877b04fb6b8cbf145b25ca
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-09-12 14:04:34 +0200

    Done: "Inbox".

--- app/routes/Apps/Inbox/Inbox.js
+++ app/routes/Apps/Inbox/Inbox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import faker from 'faker';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
 import { 
     Container,
@@ -25,6 +26,7 @@ import {
 
 import { MailboxLeftNav } from "../../components/Mailbox/MailboxLeftNav";
 import { Paginations } from "../../components/Paginations";
+import { TrTableInbox } from "./components/TrTableInbox";
 
 const Inbox = () => (
     <React.Fragment>
@@ -106,86 +108,19 @@ const Inbox = () => (
                                 </tr>
                             </thead>
                             <tbody>
-                                { /* START TR */}
-                                <tr>
-                                    <td className="align-middle">
-                                        <i className="fa fa-circle fa-fw text-primary" id="tooltipNewMessage"></i>
-                                        <UncontrolledTooltip placement="bottom" target="tooltipNewMessage">
-                                            New Message
-                                        </UncontrolledTooltip>
-                                    </td>
-                                    <td className="align-middle">
-                                        <CustomInput type="checkbox" id="mailboxCheckbox" label="" inline />
-                                    </td>
-                                    <td className="align-middle">
-                                        <Media>
-                                            <Media left className="d-flex align-self-center mr-3">
-                                                <div className="mr-2">
-                                                    <a href="#" id="tooltipAddToFavorites">
-                                                        <i className="fa fa-fw fa-star-o"></i>
-                                                    </a>
-                                                    <UncontrolledTooltip placement="top" target="tooltipAddToFavorites">
-                                                        Add To Favorites
-                                                    </UncontrolledTooltip>
-                                                </div>
-                                                <Avatar.Image
-                                                    size="md"
-                                                    src="http://bs4.webkom.co/img/avatars/2.jpg"
-                                                    addOns={[
-                                                        <AvatarAddOn.Icon 
-                                                            className="fa fa-circle"
-                                                            color="white"
-                                                            key="avatar-icon-bg"
-                                                        />,
-                                                        <AvatarAddOn.Icon 
-                                                            className="fa fa-circle"
-                                                            color="danger"
-                                                            key="avatar-icon-fg"
-                                                        />
-                                                    ]}
-                                                /> 
-                                            </Media>
-                                            <Media body>
-                                                <div className="mt-0 d-flex">
-                                                    { faker.name.firstName() } { faker.name.lastName() }
-                                                </div>
-                                                <span>
-                                                    { faker.address.state() }
-                                                </span>
-                                            </Media>
-                                        </Media>
-                                    </td>
-                                    <td className="align-middle">
-                                        <Link to="/apps/email-details">
-                                            { faker.company.catchPhrase() }
-                                        </Link>
-                                        <br />
-                                        { faker.lorem.sentence() }
-                                    </td>
-                                    <td className="align-middle">
-                                        <Badge pill color="primary" outline>
-                                            { faker.commerce.department() }
-                                        </Badge>
-                                    </td>
-                                    <td className="align-middle">
-                                        <i className="fa fa-paperclip"></i>
-                                    </td>
-                                    <td className="align-middle text-right">
-                                        30-Jun-2014<br />
-                                        01:54 PM
-                                    </td>
-                                </tr>
-                            { /* END TR */}
+                            {
+                                _.times(11, (index) => (
+                                    <TrTableInbox 
+                                        id={index}
+                                        key={index}
+                                    />            
+                                    ))
+                                }
                             </tbody>
                         </Table>
                         { /* END Table */}
-                        <CardFooter className="d-flex">
-                            <span className="align-self-center">
-                                Showing 1 to 10 of 57 entries
-                            </span>
-                            <span className="ml-auto">
-                                <Paginations />
-                            </span>
+                        <CardFooter className="d-flex justify-content-center pb-0">
+                            <Paginations />
                         </CardFooter>
                     </Card>
                 </Col>
---
+++ app/routes/Apps/Inbox/components/TrTableInbox.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import faker from 'faker';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { 
+    Badge,
+    Avatar,
+    UncontrolledTooltip,
+    CustomInput,
+    AvatarAddOn,
+    Media
+} from './../../../../components';
+
+import { randomArray } from './../../../../utilities';
+
+const status = [
+    "warning",
+    "danger",
+    "success",
+    "secondary"
+];
+
+
+const tag = [
+    "primary",
+    "secondary",
+    "info"
+];
+
+
+const TrTableInbox = (props) => (
+    <React.Fragment>
+        <tr>
+            <td className="align-middle">
+                {
+                    ( Math.round(Math.random()) )?(
+                        <span>
+                            <i className="fa fa-circle fa-fw text-primary" id={`newMessage-${ props.id }` }></i>
+                            <UncontrolledTooltip placement="bottom" target={`#newMessage-${ props.id }` }>
+                                New Message
+                            </UncontrolledTooltip>
+                        </span>
+                        ):(
+                             <span></span>
+                        )
+                }
+            </td>
+            <td className="align-middle">
+                <CustomInput type="checkbox" label="" id={` mailboxCheckbox-${ props.id }` } />
+            </td>
+            <td className="align-middle">
+                <Media>
+                    <Media left className="d-flex align-self-center mr-3">
+                        <div className="mr-2">
+                            <a href="#" id="tooltipAddToFavorites">
+                                <i className="fa fa-fw fa-star-o"></i>
+                            </a>
+                            <UncontrolledTooltip placement="top" target="tooltipAddToFavorites">
+                                Add To Favorites
+                            </UncontrolledTooltip>
+                        </div>
+                        <Avatar.Image
+                            size="md"
+                            src="http://bs4.webkom.co/img/avatars/2.jpg"
+                            addOns={[
+                                <AvatarAddOn.Icon 
+                                    className="fa fa-circle"
+                                    color="white"
+                                    key="avatar-icon-bg"
+                                />,
+                                <AvatarAddOn.Icon 
+                                    className="fa fa-circle"
+                                    color={ randomArray(status) }
+                                    key="avatar-icon-fg"
+                                />
+                            ]}
+                        /> 
+                    </Media>
+                    <Media body>
+                        <div className="mt-0 d-flex">
+                            { faker.name.firstName() } { faker.name.lastName() }
+                        </div>
+                        <span>
+                            { faker.address.state() }
+                        </span>
+                    </Media>
+                </Media>
+            </td>
+            <td className="align-middle">
+                <Link to="/apps/email-details">
+                    { faker.company.catchPhrase() }
+                </Link>
+                <br />
+                { faker.lorem.sentence() }
+            </td>
+            <td className="align-middle">
+                <Badge pill color={ randomArray(tag) }>
+                    { faker.commerce.department() }
+                </Badge>
+            </td>
+            <td className="align-middle">
+                <i className="fa fa-paperclip"></i>
+            </td>
+            <td className="align-middle text-right">
+                30-Jun-2014<br />
+                01:54 PM
+            </td>
+        </tr>
+
+    </React.Fragment>
+)
+
+TrTableInbox.propTypes = {
+    id: PropTypes.string
+};
+
+export { TrTableInbox };
