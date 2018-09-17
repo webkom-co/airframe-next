47efac453cd006f7872e4f95276175c3ada8862d
Tomasz O <tomasz.owczarczyk@gmail.com>
2018-09-17 17:58:38 +0200

    Update LayoutA.js
    Added description to "Layout A".

--- app/routes/Layouts/LayoutA/LayoutA.js
+++ app/routes/Layouts/LayoutA/LayoutA.js
@@ -6,12 +6,102 @@ import {
     Row,
     Col
 } from './../../../components';
+import { HeaderMain } from "../../components/HeaderMain";
 
 export const LayoutA = () => (
     <Container>
         <Row>
             <Col>
-                { faker.lorem.paragraphs() }
+                <HeaderMain 
+                    title="Layout A"
+                    className="mb-5 mt-4"
+                />
+
+                <h4>Sidebar</h4>
+                <p>
+                    This sidebar uses <strong>Routes</strong>:
+                </p>
+                <ul>
+                    <li>
+                        Top Fixed Section: <code>/app/routes/components/Sidebar/<strong>SidebarTopB</strong></code>
+                    </li>
+                    <li>
+                        Middle Section <i>(navigation)</i>: <code>/app/routes/components/Sidebar/<strong>DefaultSidebarNav</strong></code>
+                    </li>
+                    <li>
+                        Bottom Section <i>(footer)</i>: <code>/app/routes/components/Sidebar/<strong>SidebarBottomB</strong></code>
+                    </li>
+                </ul>
+                <hr/>
+                <h6>Sidebar Code Example:</h6>
+                <pre>
+                    <code>
+                    &lt;Sidebar&gt; <br /><br />
+                        &nbsp; /* START SIDEBAR-OVERLAY: Close (x) */ <br />
+                        &nbsp; &lt;Sidebar.Close&gt;<br />
+                        &nbsp; &nbsp; &lt;SidebarTrigger tag={ 'a' } href="javascript:;"&gt;<br />
+                        &nbsp; &nbsp; &nbsp; &lt;i className="fa fa-times-circle fa-fw"&gt;&lt;/i&gt;<br />
+                        &nbsp; &nbsp; &lt;/SidebarTrigger&gt;<br />
+                        &nbsp; &lt;/Sidebar.Close&gt;<br />
+                        &nbsp; /* END SIDEBAR-OVERLAY: Close (x) */<br />
+                        <br />
+                        &nbsp; /* START SIDEBAR: Fixed Section */<br />
+                        &nbsp; &lt;Sidebar.Section&gt;<br />
+                        &nbsp; &nbsp; <strong>&lt;SidebarTopB /&gt;</strong><br />
+                        &nbsp; &lt;/Sidebar.Section&gt;<br />
+                        &nbsp; /* END SIDEBAR: Fixed Section */<br />
+                        <br />
+                        &nbsp; /* START SIDEBAR: Mobile Scroll Wrapper */<br />
+                        &nbsp; &lt; Sidebar.MobileFluid&gt;<br />
+                        &nbsp; &nbsp; /* START SIDEBAR: Everywhere */<br />
+                        &nbsp; &nbsp; &lt; Sidebar.Section fluid cover&gt;<br />
+                        &nbsp; &nbsp; /* SIDEBAR: Menu */<br />
+                        &nbsp; &nbsp; <strong>&lt;DefaultSidebarNav /&gt;</strong><br />
+                        &nbsp; &nbsp; &lt;/Sidebar.Section&gt;<br />
+                        &nbsp; &nbsp; /* END SIDEBAR: Everywhere */<br />
+                        &nbsp; &nbsp; <strong>&lt;SidebarBottomB /&gt;</strong><br />
+                        &nbsp; &lt;/Sidebar.MobileFluid&gt;<br />
+                        &nbsp; /* END SIDEBAR: Mobile Scroll Wrapper */<br /><br />
+                    &lt;/Sidebar&gt;
+                    </code>
+                </pre>
+                <hr/>
+                <h4>Navbar</h4>
+                <p>
+                    This navbar uses <strong>Routes</strong>:
+                </p>
+                <ul>
+                    <li>
+                        Main Navbar <i>(top)</i>: <code>/app/layout/components/<strong>LayoutANavbar</strong></code>
+                    </li>
+                    <li>
+                        Top Right <i>(icon bell with badge)</i>: <code>/app/layout/components/<strong>NavbarActivityFeed</strong></code>
+                    </li>
+                    <li>
+                        Top Right <i>(icon envelope with badge)</i>: <code>/app/layout/components/<strong>NavbarMessages</strong></code>
+                    </li>
+                    <li>
+                        Top Right <i>(icon power off)</i>: <code>/app/layout/components/<strong>NavbarUser</strong></code>
+                    </li>
+                </ul>
+                <strong>Navbar Code Example:</strong>
+
+                <pre>
+                    <code><br />
+                        &lt;Navbar light color="none" expand="xs"&gt;<br />
+                            &nbsp; &lt;Nav navbar&gt;<br />
+                            &nbsp; &nbsp; &lt;NavItem className="mr-3"&gt;<br />
+                            &nbsp; &nbsp; &nbsp; &lt;SidebarTrigger/&gt;<br />
+                            &nbsp; &nbsp; &lt;/NavItem&gt;<br />
+                            &nbsp; &lt;/Nav&gt;<br />
+                            &nbsp; &lt;Nav navbar className="ml-auto"&gt;<br />
+                            &nbsp; &nbsp; &lt;<strong>NavbarActivityFeed</strong> /&gt;<br />
+                            &nbsp; &nbsp; &lt;<strong>NavbarMessages</strong> className="ml-2" /&gt;<br />
+                            &nbsp; &nbsp; &lt;<strong>NavbarUser</strong> className="ml-2" /&gt;<br />
+                            &nbsp; &lt;/Nav&gt;<br />
+                        &lt;/Navbar&gt;<br />
+                    </code>
+                </pre>
             </Col>
         </Row>
     </Container>
