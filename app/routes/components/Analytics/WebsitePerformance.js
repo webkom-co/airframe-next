eb3d0f11ee6b0e624834e594aa1620caab2afd87
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-09-20 14:21:59 +0200

    Start Responsive for "Website Performance".

--- app/routes/components/Analytics/WebsitePerformance.js
+++ app/routes/components/Analytics/WebsitePerformance.js
@@ -14,14 +14,14 @@ import {
 
 const WebsitePerformance = (props) => (
     <React.Fragment>
-        <Row>
-            <Col sm={ 8 } className="col-sm-8 d-flex align-items-start flex-column">
+        <Row className="flex-column-reverse flex-lg-row">
+            <Col sm={ 12 } lg={ 8 } className="d-flexalign-items-start flex-column">
                 <h6 className="mb-auto">
                     { props.title }
                 </h6>
                 <TinyAreaChart />
             </Col>
-            <Col sm={ 4 } className="text-right">
+            <Col sm={ 12 } lg={ 4 } className="text-right">
                 <h2>
                     { props.value }
                 </h2>
