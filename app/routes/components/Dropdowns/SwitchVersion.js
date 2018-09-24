0786d1b9f2861a0e5092a7aff039c2a0f6421a7b
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-09-24 14:36:03 +0200

    Added to "Switch Dropdown" "Vue" and delete "Other Products".

--- app/routes/components/Dropdowns/SwitchVersion.js
+++ app/routes/components/Dropdowns/SwitchVersion.js
@@ -3,6 +3,10 @@ import faker from 'faker';
 import { Link } from 'react-router-dom';
 
 import { 
+    Badge, 
+} from './../../../components';
+
+import { 
     DropdownMenu,
     DropdownItem
 } from './../../../components';
@@ -15,7 +19,7 @@ const SwitchVersion = () => (
             </DropdownItem>
             <DropdownItem href="http://jquery.bs4.webkom.co" className="d-flex">
                 <span>
-                    Jquery 2.0.0 <small>(npm)</small>
+                    Jquery 2.0.0
                     <br />
                     <span className="small">
                         Sun, Jun 12, 2018 4:43:12 PM
@@ -35,7 +39,7 @@ const SwitchVersion = () => (
             </DropdownItem>
             <DropdownItem href="http://angular.bs4.webkom.co" className="d-flex">
                 <span>
-                    Angular 1.0.0
+                    Angular 1.0.0  <Badge color="secondary">Alpha</Badge>
                     <br />
                     <span className="small">
                         Sun, Jun 12, 2018 4:43:12 PM
@@ -43,29 +47,15 @@ const SwitchVersion = () => (
                 </span>
                 <i className="fa fa-fw ml-auto align-self-center pl-2" />
             </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem header>
-                Other Products
-            </DropdownItem>
-            <DropdownItem href="http://bs4.webkom.co" className="d-flex">
-                <i className="fa fa-send fa-fw mr-3 align-self-center" />
+            <DropdownItem href="http://vue.bs4.webkom.co" className="d-flex">
                 <span>
-                    bs4.webkom.co <small>(Jekyll)</small>
+                    Vue 1.0.0 <Badge color="secondary">Alpha</Badge>
                     <br />
                     <span className="small">
                         Sun, Jun 12, 2018 4:43:12 PM
                     </span>
                 </span>
-            </DropdownItem>
-            <DropdownItem href="http://spin.webkom.co/v2" className="d-flex">
-                <i className="fa fa-bandcamp fa-fw mr-3 align-self-center" />
-                <span>
-                    spin.webkom.co/v2 <small>(Jekyll)</small>
-                    <br />
-                    <span className="small">
-                        Sun, Jun 12, 2018 4:43:12 PM
-                    </span>
-                </span>
+                <i className="fa fa-fw ml-auto align-self-center pl-2" />
             </DropdownItem>
         </DropdownMenu>
     </React.Fragment>
