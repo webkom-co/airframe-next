6bf2b8f8d04175c5b91a11d58ab985ee00fcc960
Tomasz Owczarczyk <tomasz.owczarczyk@gmail.com>
2018-10-02 09:11:36 +0200

    [ FAVICON ] Replaced favicon as default: "B".

--- app/layout/default.js
+++ app/layout/default.js
@@ -13,20 +13,12 @@ import {
 } from './../routes';
 
 const favIcons = [
-    { rel: 'apple-touch-icon', sizes: '57x57', href: require('./../images/favicons/apple-icon-57x57.png') },
-    { rel: 'apple-touch-icon', sizes: '60x60', href: require('./../images/favicons/apple-icon-60x60.png') },
-    { rel: 'apple-touch-icon', sizes: '72x72', href: require('./../images/favicons/apple-icon-72x72.png') },
-    { rel: 'apple-touch-icon', sizes: '76x76', href: require('./../images/favicons/apple-icon-76x76.png') },
-    { rel: 'apple-touch-icon', sizes: '114x114', href: require('./../images/favicons/apple-icon-114x114.png') },
-    { rel: 'apple-touch-icon', sizes: '120x120', href: require('./../images/favicons/apple-icon-120x120.png') },
-    { rel: 'apple-touch-icon', sizes: '144x144', href: require('./../images/favicons/apple-icon-144x144.png') },
-    { rel: 'apple-touch-icon', sizes: '152x152', href: require('./../images/favicons/apple-icon-152x152.png') },
-    { rel: 'apple-touch-icon', sizes: '180x180', href: require('./../images/favicons/apple-icon-152x152.png') },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: require('./../images/favicons/apple-touch-icon.png') },
 
-    { rel: 'icon', type: 'image/png', sizes: '192x192', href: require('./../images/favicons/android-icon-192x192.png') },
+    { rel: 'icon', type: 'image/png', sizes: '192x192', href: require('./../images/favicons/android-chrome-192x192.png') },
+    { rel: 'icon', type: 'image/png', sizes: '256x256', href: require('./../images/favicons/android-chrome-256x256.png') },
 
     { rel: 'icon', type: 'image/png', sizes: '32x32', href: require('./../images/favicons/favicon-32x32.png') },
-    { rel: 'icon', type: 'image/png', sizes: '96x96', href: require('./../images/favicons/favicon-96x96.png') },
     { rel: 'icon', type: 'image/png', sizes: '16x16', href: require('./../images/favicons/favicon-16x16.png') }
 ];
 
