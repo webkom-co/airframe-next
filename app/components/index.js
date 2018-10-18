83c5c79849b5da16ffbad4d3d56060b1649d568b
unski11ed <maciej.brzeczyk@gmail.com>
2018-10-18 16:03:20 +0200

    Added Dropzone example

--- app/components/index.js
+++ app/components/index.js
@@ -5,6 +5,7 @@ import Card from './Card';
 import CardHeader from './CardHeader';
 import Checkable from './Checkable';
 import CustomInput from './CustomInput';
+import Divider from './Divider';
 import EmptyLayout from './EmptyLayout';
 import ExtendedDropdown from './ExtendedDropdown';
 import HolderProvider from './HolderProvider';
@@ -107,6 +108,7 @@ export {
     CardHeader,
     Checkable,
     CustomInput,
+    Divider,
     EmptyLayout,
     ExtendedDropdown,
     IconWithBadge,
