6355ee0bb6b9d4c61d49ae4a5e458aa98d3c6ce6
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-06-10 11:50:29 +0200

    Layouts: links text-primary

--- pages/layouts/navbar-only.js
+++ pages/layouts/navbar-only.js
@@ -10,8 +10,8 @@ import { LayoutNavbarOnly } from
 const NavbarOnly = () => (
     <Container>
         <p className="mb-5 mt-3">
-            Welcome to the <b>&quot;Airframe&quot;</b> Admin Dashboard Theme based on <a href="https://getbootstrap.com" target="_blank" rel="noopener noreferrer">Bootstrap 4.x</a> version for React called&nbsp;
-            <a href="https://reactstrap.github.io" target="_blank" rel="noopener noreferrer">reactstrap</a> - easy to use React Bootstrap 4 components compatible with React 16+.
+            Welcome to the <b>&quot;Airframe&quot;</b> Admin Dashboard Theme based on <a href="https://getbootstrap.com" className="text-primary" target="_blank" rel="noopener noreferrer">Bootstrap 4.x</a> version for React called&nbsp;
+            <a href="https://reactstrap.github.io" className="text-primary" target="_blank" rel="noopener noreferrer">reactstrap</a> - easy to use React Bootstrap 4 components compatible with React 16+.
         </p>
 
         <section className="mb-5">
@@ -20,10 +20,10 @@ const NavbarOnly = () => (
             </h6>
             <ul className="pl-3">
                 <li>
-                    <Link to="/layouts/navbar-only">Navbar Only</Link>
+                    <Link to="/layouts/navbar-only" className="text-primary">Navbar Only</Link>
                 </li>
                 <li>
-                    <Link to="/layouts/sidebar-default">Sidebar with Navbar</Link>
+                    <Link to="/layouts/sidebar-default" className="text-primary">Sidebar with Navbar</Link>
                 </li>
             </ul>
         </section>
@@ -34,13 +34,13 @@ const NavbarOnly = () => (
             </h6>
             <ul className="pl-3">
                 <li>
-                    <a href="https://webkom.gitbook.io/spin/v/airframe/airframe-next.js/documentation-next.js" target="_blank" rel="noopener noreferrer">Documentation</a> - which describes how to configure this version.
+                    <a href="https://webkom.gitbook.io/spin/v/airframe/airframe-next.js/documentation-next.js" className="text-primary" target="_blank" rel="noopener noreferrer">Documentation</a> - which describes how to configure this version.
                 </li>
                 <li>
-                    <a href="https://webkom.gitbook.io/spin/v/airframe/airframe-next.js/credits-next.js" target="_blank" rel="noopener noreferrer">Credits</a> - technical details of which versions are used for this framework.
+                    <a href="https://webkom.gitbook.io/spin/v/airframe/airframe-next.js/credits-next.js" className="text-primary" target="_blank" rel="noopener noreferrer">Credits</a> - technical details of which versions are used for this framework.
                 </li>
                 <li>
-                    <a href="https://webkom.gitbook.io/spin/v/airframe/airframe-next.js/roadmap-react" target="_blank" rel="noopener noreferrer">Roadmap</a> - update for this technology for the coming months.
+                    <a href="https://webkom.gitbook.io/spin/v/airframe/airframe-next.js/roadmap-react" className="text-primary" target="_blank" rel="noopener noreferrer">Roadmap</a> - update for this technology for the coming months.
                 </li>
                 <li>
                     <b>Bugs</b> - do you see errors in this version? Please report vie email: <i>info@webkom.co</i>
@@ -54,22 +54,22 @@ const NavbarOnly = () => (
             </h6>
             <ul className="pl-3">
                 <li>
-                    <a href="http://dashboards.webkom.co/jquery/airframe">jQuery</a> - based on the newest <i>Bootstrap 4.x</i>
+                    <a href="http://dashboards.webkom.co/jquery/airframe" className="text-primary">jQuery</a> - based on the newest <i>Bootstrap 4.x</i>
                 </li>
                 <li>
-                    <a href="http://dashboards.webkom.co/react/airframe">React</a> - based on the newest <i>Reactstrap</i>
+                    <a href="http://dashboards.webkom.co/react/airframe" className="text-primary">React</a> - based on the newest <i>Reactstrap</i>
                 </li>
                 <li>
-                    <a href="http://dashboards.webkom.co/react-next/airframe">Next.js (React)</a> - based on the newest <i>Reactstrap</i> and <i>Next.js</i>
+                    <a href="http://dashboards.webkom.co/react-next/airframe" className="text-primary">Next.js (React)</a> - based on the newest <i>Reactstrap</i> and <i>Next.js</i>
                 </li>
                 <li>
-                    <a href="http://dashboards.webkom.co/angular/airframe">Angular</a> - based on the newest <i>ng-bootstrap</i>
+                    <a href="http://dashboards.webkom.co/angular/airframe" className="text-primary">Angular</a> - based on the newest <i>ng-bootstrap</i>
                 </li>
                 <li>
-                    <a href="http://dashboards.webkom.co/net-mvc/airframe">.NET MVC</a> - based on the newest <i>Bootstrap 4.x</i>
+                    <a href="http://dashboards.webkom.co/net-mvc/airframe"> className="text-primary".NET MVC</a> - based on the newest <i>Bootstrap 4.x</i>
                 </li>
                 <li>
-                    <a href="http://dashboards.webkom.co/vue/airframe">Vue.js</a> - based on the newest <i>BootstrapVue</i>
+                    <a href="http://dashboards.webkom.co/vue/airframe" className="text-primary">Vue.js</a> - based on the newest <i>BootstrapVue</i>
                 </li>
                 <li>
                     <b>Other Versions</b>, such as <i>Ruby on Rails, Ember, Laravel etc.</i>, please ask for the beta version via email: info@webkom.co
@@ -83,7 +83,7 @@ const NavbarOnly = () => (
             </h6>
             <p>
                 Regarding configuration, changes under client&apos;s requirements.<br />
-                Pleace contact us through the <a href="http://wbkom.co/contact" target="_blank" rel="noopener noreferrer">webkom.co/contact</a> website.
+                Pleace contact us through the <a href="http://wbkom.co/contact" className="text-primary" target="_blank" rel="noopener noreferrer">webkom.co/contact</a> website.
             </p>
         </section>
     </Container>
