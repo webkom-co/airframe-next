84a33b2a964d0e15ffcf2cab498703b65ea047cd
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-06-12 11:15:05 +0200

    Fixed DatePicker

--- features/DatePicker/ButtonInput.js
+++ features/DatePicker/ButtonInput.js
@@ -5,16 +5,19 @@ import {
     Button
 } from './../../components';
 
-export const ButtonInput = (props) => (
+// eslint-disable-next-line react/display-name
+const ButtonInputFR = React.forwardRef((props, ref) => (
     <Button outline
         onClick={props.onClick}
+        ref={ref}
     >
         <i className="fa fa-fw fa-calendar-o mr-1" />
         { props.value }
     </Button>
-)
-
-ButtonInput.propTypes = {
+));
+ButtonInputFR.propTypes = {
     onClick: PropTypes.func,
     value: PropTypes.string
 }
+
+export { ButtonInputFR as ButtonInput };
