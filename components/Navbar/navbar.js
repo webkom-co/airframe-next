ad7f44f7d298073468d7c21e7505f35970e2df8b
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-08-05 14:35:48 +0200

    Navbar: Added new theming options

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -3,14 +3,40 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { Navbar as BSNavbar, Container } from 'reactstrap';
 
-const Navbar = ({ themed, fluid, shadow, className, children, ...otherProps }) => {
-    const navbarClass = classNames({
-        'navbar-themed': themed,
+const Navbar = ({
+    themed,
+    fluid,
+    shadow,
+    className,
+    children,
+    dark,
+    light,
+    color,
+    ...otherProps
+}) => {
+    let navbarClass = classNames({
+        'navbar-themed': themed || !!color,
         'navbar-shadow': shadow,
     }, 'navbar-multi-collapse', className);
 
+    // When a combination of light or dark is present
+    // with a color - use a custom class instead of bootstrap's
+    if ((dark || light) && color) {
+        navbarClass = classNames(navbarClass,
+            `navbar-${light ? 'light' : '' }${dark ? 'dark' : ''}-${color}`);
+    }
+
     return (
-        <BSNavbar className={ navbarClass } { ...otherProps }>
+        <BSNavbar
+            className={ navbarClass }
+            /*
+                Use the dark and light switches
+                only when color is not set
+            */
+            dark={ dark && !color }
+            light={ light && !color }
+            { ...otherProps }
+        >
             {
                 <Container className="navbar-collapse-wrap" fluid={ fluid }>
                     { children }
@@ -25,6 +51,9 @@ Navbar.propTypes = {
     shadow: PropTypes.bool,
     className: PropTypes.string,
     children: PropTypes.node,
+    color: PropTypes.string,
+    dark: PropTypes.bool,
+    light: PropTypes.bool
 }
 Navbar.defaultProps = {
     fluid: false,
